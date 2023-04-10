a2a4680738855fe08897175a5286919e739f276b
cbmarie255 <court.b1993@gmail.com>
2023-04-10 14:51:44 -0500

    adding setup for Docker environment

---
+++ devportal/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Upgrade system and install the following packages:
+apt update
+apt upgrade -y
+apt install -y make zip docker.io awscli
+
+# # Download Go
+wget https://go.dev/dl/go1.20.1.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.20.1.linux-amd64.tar.gz
+rm ./go1.20.1.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+# Download the Hugo executable
+wget https://github.com/gohugoio/hugo/releases/download/v0.110.0/hugo_0.110.0_Linux-64bit.tar.gz
+tar -C /usr/local/bin -xzf hugo_0.110.0_Linux-64bit.tar.gz
+rm ./hugo_0.110.0_Linux-64bit.tar.gz
